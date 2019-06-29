1797ac1a146a172a83363c7b8c9c18f8e8e1be38
Pablo Hernandez <prh90@yahoo.com>
2019-06-28 22:45:15 -0700

    Adds searchbar file and includes it in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './SearchBar';
+
+
+class App extends React.Component{
+  render(){
+    return (
+      <div className="ui container">
+        <SearchBar/>
+      </div>
+    );
+  }
+}
+
+export default App;
